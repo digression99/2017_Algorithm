022ba1c1df7bfea3eeb4d5565205a558e810c100
digression99 <digression99@gmail.com>
2017-07-14 15:58:19 +0900

    Update baekjoon_2331.cpp

--- baekjoon_2331.cpp
+++ baekjoon_2331.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <map>
+#include <cmath>
 
+int main()
+{
+    int A, p, sum, k, i = 0;
+    map<int, int> dic;
+    
+    cin >> A >> p;
+    
+    k = sum = A;
+    
+    while (!dic.count(sum))
+    {
+        dic[sum] = i++;
+        k = sum;
+        sum = 0;
+        while (k > 0)
+        {
+            sum += pow(k % 10, p);
+            k /= 10;
+        }
+    }
+    
+    cout << dic[sum] << endl;
+    
+    return 0;
+}
