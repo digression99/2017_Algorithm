aa779e01a7f66f308bbb6d8463e2c03cbc7de425
digression99 <digression99@gmail.com>
2017-09-06 22:12:07 +0900

    Update topcoder_friendscore_2.cpp

--- topcoder_friendscore_2.cpp
+++ topcoder_friendscore_2.cpp
@@ -14,54 +14,84 @@ using namespace std;
 
 class FriendScore{
 public:
-    void checkConnection(vector<string> & friends, int src, int dest, vector<int> & twoFriends)
-    {
-        int l = (int)friends.size();
-        for (int i = src + 1; i < l; ++i)
-        {
-            //if (friends[dest][i] == 'C' || friends[dest][i] == 'Y')
-            if (friends[dest][i] == 'Y' || friends[dest][i] == 'C')
-            {
-                // 'Y' -> not yet checked, has a direct connection.
-                // 'C' -> checked, or has a indirect connection.
-            
-                if (friends[i][src] == 'N') // if they had a connection, it already changed to 'C'.
-                { // need to handle 'C' here.
-                    friends[i][src] = friends[src][i] = 'C';
-                    twoFriends[i]++;
-                    twoFriends[src]++;
-                }
-            }
-        }
-    }
-    
+//    void checkConnection(vector<string> & friends, int src, int dest, vector<int> & twoFriends)
+//    {
+//        int l = (int)friends.size();
+//        for (int i = src + 1; i < l; ++i)
+//        {
+//            //if (friends[dest][i] == 'C' || friends[dest][i] == 'Y')
+//            if (friends[dest][i] == 'Y' || friends[dest][i] == 'C')
+//            {
+//                // 'Y' -> not yet checked, has a direct connection.
+//                // 'C' -> checked, or has a indirect connection.
+//                
+//                if (friends[i][src] == 'N') // if they had a connection, it already changed to 'C'.
+//                { // need to handle 'C' here.
+//                    friends[i][src] = friends[src][i] = 'C';
+//                    twoFriends[i]++;
+//                    twoFriends[src]++;
+//                }
+//            }
+//        }
+//    }
     int highestScore(vector<string> friends)
     {
-        vector<int> twoFriends;
+        int mx = 0;
         int l = (int)friends.size();
-        
-        twoFriends.resize(l);
         
         for (int i = 0; i < l; ++i)
         {
+            int cnt = 0;
+            
             for (int j = 0; j < l; ++j)
             {
-                if (i == j) continue;
-                
-                if (friends[i][j] == 'Y' || friends[i][j] == 'C')
+                if (i != j)
                 {
-                    if (friends[i][j] == 'Y')
-                    {
-                        twoFriends[i]++;
-                        twoFriends[j]++;
-                        friends[i][j] = friends[j][i] = 'C';
+                    if (friends[i][j] == 'Y') cnt++;
+                    else {
+                        for (int k = 0; k < l; ++k)
+                        {
+                            if (friends[j][k] == 'Y' && friends[k][i] == 'Y')
+                            {
+                                cnt++;
+                                break;
+                            }
+                        }
                     }
-                    checkConnection(friends, i, j, twoFriends);
                 }
             }
+            mx = std::max(mx, cnt);
         }
-        return *std::max_element(twoFriends.begin(), twoFriends.end());
+        return mx;
     }
+    
+//    int highestScore(vector<string> friends)
+//    {
+//        vector<int> twoFriends;
+//        int l = (int)friends.size();
+//        
+//        twoFriends.resize(l);
+//        
+//        for (int i = 0; i < l; ++i)
+//        {
+//            for (int j = 0; j < l; ++j)
+//            {
+//                if (i == j) continue;
+//                
+//                if (friends[i][j] == 'Y' || friends[i][j] == 'C')
+//                {
+//                    if (friends[i][j] == 'Y')
+//                    {
+//                        twoFriends[i]++;
+//                        twoFriends[j]++;
+//                        friends[i][j] = friends[j][i] = 'C';
+//                    }
+//                    checkConnection(friends, i, j, twoFriends);
+//                }
+//            }
+//        }
+//        return *std::max_element(twoFriends.begin(), twoFriends.end());
+//    }
 };
 
 
@@ -70,20 +100,20 @@ vector<vector<string> > friendsVecs = {
     {"NYY", "YNY", "YYN"},
     {"NYNNN", "YNYNN", "NYNYN", "NNYNY", "NNNYN"},
     {"NNNNYNNNNN",
-     "NNNNYNYYNN",
-     "NNNYYYNNNN",
-     "NNYNNNNNNN",
-     "YYYNNNNNNY",
-     "NNYNNNNNYN",
-     "NYNNNNNYNN",
-     "NYNNNNYNNN",
-     "NNNNNYNNNN",
-     "NNNNYNNNNN"},
+        "NNNNYNYYNN",
+        "NNNYYYNNNN",
+        "NNYNNNNNNN",
+        "YYYNNNNNNY",
+        "NNYNNNNNYN",
+        "NYNNNNNYNN",
+        "NYNNNNYNNN",
+        "NNNNNYNNNN",
+        "NNNNYNNNNN"},
     {"NNNNNNNNNNNNNNY",
-     "NNNNNNNNNNNNNNN",
-     "NNNNNNNYNNNNNNN",
-     "NNNNNNNYNNNNNNY",
-     "NNNNNNNNNNNNNNY", "NNNNNNNNYNNNNNN", "NNNNNNNNNNNNNNN", "NNYYNNNNNNNNNNN", "NNNNNYNNNNNYNNN", "NNNNNNNNNNNNNNY", "NNNNNNNNNNNNNNN", "NNNNNNNNYNNNNNN", "NNNNNNNNNNNNNNN", "NNNNNNNNNNNNNNN", "YNNYYNNNNYNNNNN"},
+        "NNNNNNNNNNNNNNN",
+        "NNNNNNNYNNNNNNN",
+        "NNNNNNNYNNNNNNY",
+        "NNNNNNNNNNNNNNY", "NNNNNNNNYNNNNNN", "NNNNNNNNNNNNNNN", "NNYYNNNNNNNNNNN", "NNNNNYNNNNNYNNN", "NNNNNNNNNNNNNNY", "NNNNNNNNNNNNNNN", "NNNNNNNNYNNNNNN", "NNNNNNNNNNNNNNN", "NNNNNNNNNNNNNNN", "YNNYYNNNNYNNNNN"},
 };
 
 vector<int> resVecs = {
